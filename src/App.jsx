5a5dbad07ef6b6c0a57ca3c655a92e63a6ed0d08
dee797 <178827694+dee797@users.noreply.github.com>
2024-11-26 12:20:23 -0700

    Use React Router to link between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 
 const App = () => {
@@ -10,6 +11,19 @@ const App = () => {
 
   return (
     <>
+      <div>
+        <h1>Hello from the main page of the app!</h1>
+        <p>Here are some examples of links to other pages</p>
+        <nav>
+          <ul>
+            <li>
+              <Link to="profile">Profile page</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      { /* Use for Vitest/React Testing/Jest */ }
       <button type="button" onClick={clickHandler}>
         Click Me
       </button>
@@ -18,13 +32,15 @@ const App = () => {
   );
 };
 
-
+// RenderName component
 const RenderName = (props) => {
   return <div>{props.name}</div>;
 };
 
+
+// Set data types on RenderName props
 RenderName.propTypes = {
-  name: PropTypes.string,isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 RenderName.defaultProps = {
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.css';
+import App from './App';
+import {Profile, DefaultProfile} from "./Profile";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile",
+    element: <Profile />,
+    children: [
+      { index: true, element: <DefaultProfile /> },
+      { path: "spinach", element: <Spinach /> },
+      { path: "popeye", element: <Popeye /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
