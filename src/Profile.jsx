a78dacc51e88115d06412694492e4ad985f6905b
dee797 <178827694+dee797@users.noreply.github.com>
2024-11-26 12:18:54 -0700

    Create profile pages

---
+++ src/Profile.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Hello from profile page!</h1>
+      <p>So, how are you?</p>
+      <hr />
+      <h2>The profile visited is here:</h2>
+      <Outlet />
+    </div>
+  );
+};
+
+const DefaultProfile = () => {
+    return <p>Oh, nothing to see here!</p>;
+  };
+
+export {Profile, DefaultProfile};
