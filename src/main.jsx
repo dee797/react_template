b0b1a057838939d6e4baa0120a62390540409021
dee797 <178827694+dee797@users.noreply.github.com>
2024-11-26 15:02:01 -0700

    Use dynamic segments and refactor routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css';
-import App from './App';
-import {Profile, DefaultProfile} from "./Profile";
-import Spinach from "./Spinach";
-import Popeye from "./Popeye";
+import routes from "./routes";
+import "./index.css"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
